bc7b6067403645ca4a31c77c1d7b680a86e5a546
jlcoulter <104843199+jlcoulter@users.noreply.github.com>
2022-05-20 21:41:30 +1000

    Update qtile.sh

--- qtile.sh
+++ qtile.sh
@@ -1,2 +1,57 @@
-dnf copr enable frostyx/qtile -y
-dnf install qtile qtile-extras -y
+#RPM
+sudo dnf install https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+
+#Qtile
+sudo dnf copr enable frostyx/qtile -y
+sudo dnf install qtile qtile-extras -y
+
+#Terminal, File Explorer
+sudo dnf install alacritty fish pcmanfm firefox vim nitrogen neofetch awesome dmenu polkit picom sddm stacer snapd flameshot -y
+
+#dnf-plugins-core
+sudo dnf install dnf-plugins-core -y
+
+#Brave
+sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
+sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+sudo dnf install brave-browser -y
+
+#DevTools and Extension Support
+sudo dnf install @development-tools -y
+
+#VS Code
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+cat <<EOF | sudo tee /etc/yum.repos.d/vscode.repo
+[code]
+name=Visual Studio Code
+baseurl=https://packages.microsoft.com/yumrepos/vscode
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.microsoft.com/keys/microsoft.asc
+EOF
+sudo dnf install code -y
+
+#Steam
+sudo dnf install steam -y
+
+#Gimp
+sudo dnf install gimp -y
+
+#RawTherapee
+sudo dnf install rawtherapee -y
+
+#KDENlive
+sudo dnf install kdenlive -y
+
+#Spotify
+sudo ln -s /var/lib/snapd/snap /snap
+sudo snap install spotify
+
+#Discord
+sudo dnf install discord -y
+
+#Graphical Target
+sudo systemctl enable sddm
+sudo systemctl set-default graphical.target
+reboot
