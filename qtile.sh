974cb37013988830e4bbd8226024c107fc575351
jlcoulter <104843199+jlcoulter@users.noreply.github.com>
2022-05-25 06:25:05 -0400

    Update qtile.sh

--- qtile.sh
+++ qtile.sh
@@ -20,6 +20,18 @@ sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.co
 sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
 sudo dnf install brave-browser -y
 
+#VS Code
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+cat <<EOF | sudo tee /etc/yum.repos.d/vscode.repo
+[code]
+name=Visual Studio Code
+baseurl=https://packages.microsoft.com/yumrepos/vscode
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.microsoft.com/keys/microsoft.asc
+EOF
+sudo dnf install code -y
+
 #Steam
 sudo dnf install steam -y
 
