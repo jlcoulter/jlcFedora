e23ea25a58704bd40e8dda1543a72e5134bed34e
jlcoulter <104843199+jlcoulter@users.noreply.github.com>
2023-05-26 22:41:49 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 sudo dnf copr enable th3-s4lm0n/leftwm -y 
-sudo dnf install leftwm polybar nnn picom rofi alacritty neovim -y -y
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+dnf check-update
+sudo dnf install leftwm polybar nnn picom rofi alacritty neovim code -y -y
 sudo dnf remove xfce4-terminal -y
+sudo dnf install code
 reboot
