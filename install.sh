25513020e91ddc86970fee258a5d63970127a546
jlcoulter <104843199+jlcoulter@users.noreply.github.com>
2023-06-06 13:21:40 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 sudo dnf copr enable th3-s4lm0n/leftwm -y 
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-sudo dnf install leftwm polybar nnn picom rofi alacritty neovim code flatpak go python zsh -y -y
+sudo dnf install leftwm polybar nnn picom rofi alacritty neovim code flatpak go python zsh thunar -y -y
 sudo dnf remove xfce4-terminal nano -y
 mkdir ~/.config/leftwm ~/.config/leftwm/themes ~/.config/autostart
 mv config.ron ~/.config/leftwm/
