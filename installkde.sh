048dbe5a3551068a4d5dbb98621c494dff1630a0
jlcoulter <104843199+jlcoulter@users.noreply.github.com>
2023-06-16 14:27:10 +1000

    Delete installkde.sh

--- installkde.sh
+++
@@ -1,6 +0,0 @@
-sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-sudo dnf install alacritty neovim code go zsh -y -y
-sudo dnf remove xfce4-terminal nano -y
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/JetBrains/JetBrainsMono/master/install_manual.sh)"
